c817bd9fb84396441d37acdb15a2803a90b58f8f
Pablo Felipe Leonhart <pablo.fl11@gmail.com>
2022-12-17 21:05:32 +0100

    Removing dist folder

--- dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from './infra/prisma.service';
-import { CreateNotificationBody } from './infra/create-notification-body';
-export declare class AppController {
-    private readonly prisma;
-    constructor(prisma: PrismaService);
-    list(): import(".prisma/client").PrismaPromise<import(".prisma/client").Notification[]>;
-    create(body: CreateNotificationBody): Promise<void>;
-}
